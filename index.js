09f807e59ffbe74c6a65d087e56809c37e5e28fd
Aleksey Tsaplin <alekseyt@ecobee.com>
2018-11-28 17:21:54 -0500

    added mongo package, git ignore for config details

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+var env = process.env.NODE_ENV || 'production';
+var config = require('./config')[env];
+
+MongoClient.connect(config.server.port, (err, database) => {
+    // ... start the server
+})
 
 app.listen(3000, function () {
     console.log('listening on 3000')
